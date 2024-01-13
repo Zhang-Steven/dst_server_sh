f05ce0952b850f6250f94fe8232a095a1cb15422
steven <steven_zhang@sjtu.edu.cn>
2024-01-13 20:37:24 +0800

    first commit ```

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sudo add-apt-repository multiverse
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install libstdc++6 libgcc1 libcurl4-gnutls-dev:i386 lib32z1
+
 mkdir ~/steamcmd
 cd ~/steamcmd
 
