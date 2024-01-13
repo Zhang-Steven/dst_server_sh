ded3f9ad7400f18d56e17728f7f5fd7c52e5b23c
steven <steven_zhang@sjtu.edu.cn>
2024-01-13 21:59:53 +0800

    updated

--- init.sh
+++ init.sh
@@ -13,3 +13,5 @@ wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 tar -xvzf steamcmd_linux.tar.gz
 
 ./steamcmd.sh +force_install_dir ~/dontstarvetogether_dedicated_server +login anonymous +app_update 343050 validate +quit
+
+mkdir -p ~/.klei/DoNotStarveTogether/
